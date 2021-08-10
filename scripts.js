b3a8c4c5f4d373c1c9473d9a5d673b843ce29e74
timvvoodman <71102666+timvvoodman@users.noreply.github.com>
2021-08-10 08:38:05 -0400

    initial files and compile watch set up

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var character = 'mario';
+console.log(character);
+var input = document.querySelectorAll('input');
+console.log(input);
+input.forEach(function (inp) {
+    console.log(inp);
+});
---
+++ scripts.ts
@@ -0,0 +1,11 @@
+const character = 'mario'
+
+console.log(character)
+
+const input = document.querySelectorAll('input')
+
+console.log(input)
+
+input.forEach((inp) => {
+  console.log(inp)
+})
